43361d74ae950b9ba2bef5f9bf4f90054aef2e5e
John Verrone <john.verrone@gmail.com>
2016-07-06 18:59:33 -0400

    removed materialize and added material design light

--- public/src/app.js
+++ public/src/app.js
@@ -3,7 +3,7 @@ export class App {
     config.title = 'ATL-RLC';
     config.map([
       // { route: ['','home'], name: 'home', moduleId: './home/home', nav: false, title:'Home' },
-      { route: ['', 'teams'], name: 'teams', moduleId: './teams/teams', nav: true, title:'Teams' }
+      { route: 'teams', name: 'teams', moduleId: './teams/teams', nav: true, title:'Teams' }
     ]);
 
     this.router = router;
